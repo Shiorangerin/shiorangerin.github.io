62bae94c0fa280334238b04f26851cdaaf7f54b6
Orangerin <141531482+Shiorangerin@users.noreply.github.com>
2026-06-20 14:18:33 +0800

    chore: deploy site via docs/ folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://shiorangerin.github.io",
+  base: "/shiorangerin",
+  outDir: "./docs",
   integrations: [sitemap()],
   devToolbar: { enabled: false },
   markdown: {
